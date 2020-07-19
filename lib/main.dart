2946bc75cc33ca23a0b1a6a0763471a16762b180
sarabpreet992 <sarabpreet992@gmail.com>
2020-07-19 16:04:10 +0530

    Flutter rows ch-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,12 +11,96 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Awesome app"),
+      appBar: AppBar(
+        title: Text("Awesome app"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                width: 100,
+                height: 100,
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    gradient: LinearGradient(
+                      colors: [Colors.green, Colors.pink],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                        offset: Offset(3.0, 6.0),
+                      )
+                    ]),
+                child: Text(
+                  "Hello world",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.w300),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              padding: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  gradient: LinearGradient(
+                    colors: [Colors.green, Colors.pink],
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5,
+                      offset: Offset(3.0, 6.0),
+                    )
+                  ]),
+              child: Text(
+                "Hello world",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    gradient: LinearGradient(
+                      colors: [Colors.green, Colors.pink],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                        offset: Offset(3.0, 6.0),
+                      )
+                    ]),
+                child: Text(
+                  "Hello world",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.w300),
+                )),
+          ],
         ),
-        body: Container(
-          child: Text("hi flutter"),
-          color: Colors.green,
-        ));
+      ),
+    );
   }
 }
