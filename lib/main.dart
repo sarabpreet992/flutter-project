f13e53d0b697b5b95da6eb547cd85b68fec439b8
sarabpreet992 <sarabpreet992@gmail.com>
2020-07-20 19:31:30 +0530

    chapter 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,94 +14,100 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome app"),
       ),
-      body: Container(
-        color: Colors.black,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-                width: 100,
-                height: 100,
-                padding: EdgeInsets.all(10),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    gradient: LinearGradient(
-                      colors: [Colors.green, Colors.pink],
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 5,
-                        offset: Offset(3.0, 6.0),
-                      )
-                    ]),
-                child: Text(
-                  "Hello world",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.w300),
-                )),
-            Container(
-              width: 100,
-              height: 100,
-              padding: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  gradient: LinearGradient(
-                    colors: [Colors.green, Colors.pink],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            color: Colors.black,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    width: 100,
+                    height: 100,
+                    padding: EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        gradient: LinearGradient(
+                          colors: [Colors.yellow, Colors.pink],
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5,
+                            offset: Offset(3.0, 6.0),
+                          )
+                        ]),
+                    child: Text(
+                      "Hello world",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.w300),
+                    )),
+                Container(
+                  width: 100,
+                  height: 100,
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      gradient: LinearGradient(
+                        colors: [Colors.green, Colors.pink],
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(3.0, 6.0),
+                        )
+                      ]),
+                  child: Text(
+                    "Hello world",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w300),
                   ),
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 5,
-                      offset: Offset(3.0, 6.0),
-                    )
-                  ]),
-              child: Text(
-                "Hello world",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.blueGrey,
-                    fontWeight: FontWeight.w300),
-              ),
+                ),
+                Container(
+                    width: 100,
+                    height: 100,
+                    padding: EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        gradient: LinearGradient(
+                          colors: [Colors.green, Colors.pink],
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5,
+                            offset: Offset(3.0, 6.0),
+                          )
+                        ]),
+                    child: Text(
+                      "Hello world",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.w300),
+                    )),
+              ],
             ),
-            Container(
-                width: 100,
-                height: 100,
-                padding: EdgeInsets.all(10),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    gradient: LinearGradient(
-                      colors: [Colors.green, Colors.pink],
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 5,
-                        offset: Offset(3.0, 6.0),
-                      )
-                    ]),
-                child: Text(
-                  "Hello world",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.blueGrey,
-                      fontWeight: FontWeight.w300),
-                )),
-          ],
+          ),
         ),
       ),
     );
